aaf462ee41b2579985570fce69024aefda1c0885
Ahmet Dayi <ahmet_dayi@eskisehir.edu.tr>
2023-02-06 22:08:23 +0300

    created project

---
+++ src/test/java/com/ahmetdayi/patikaweatherapi/PatikaWeatherApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmetdayi.patikaweatherapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatikaWeatherApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
